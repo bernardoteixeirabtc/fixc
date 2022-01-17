d5763399d2f25f000fce23162a39ed707cd7de4a
bernardo <gontijo.bernardo@gmail.com>
2022-01-16 21:53:57 -0300

    test

--- main.go
+++ main.go
@@ -95,7 +95,7 @@ func onError(err error) {
     return 
 }
 
-func SendTrade(s string, request RpcRequest) (orderId interface{}) {
+func SendTrade(s string, request RpcRequest) {
     var err error
     /* e.g. FTX application interface: new order 
         8=FIX.4.2|9=150|35=D|49=XXXX|56=FTX|34=2|21=1|52=20201111-03:17:14.349|
