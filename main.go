831be562eac84ed7276b12a3d9221ce96c98fae9
bernardo <gontijo.bernardo@gmail.com>
2022-01-17 17:50:20 -0300

    test

--- main.go
+++ main.go
@@ -105,7 +105,7 @@ func tapiCall(method string, params map[string]string) (data interface{}){
     return
 }
 
-func placeCustomTrade(contract string, tradeSide string, label string, amount float64, price float64, postOnly bool){
+func placeCustomTrade(contract string, tradeSide string, label string, amount float64, price float64, postOnly bool, (data interface{}){
     var err error
     /* e.g. FTX application interface: new order 
         8=FIX.4.2|9=150|35=D|49=XXXX|56=FTX|34=2|21=1|52=20201111-03:17:14.349|
